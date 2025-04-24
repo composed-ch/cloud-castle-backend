d8ffd7a2defe27d24e40c22f890253aedf1172e6
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-24 16:01:57 +0200

    register api key for existing account

---
+++ cmd/add-api-key/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/composed-ch/cloud-castle-backend/internal/config"
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	username := flag.String("username", "", "the unique name of the user")
+	zone := flag.String("zone", "", "the zone for which the key is used")
+	apiKey := flag.String("key", "", "API key")
+	apiSecret := flag.String("secret", "", "API secret")
+	flag.Parse()
+
+	cfg := config.MustReadConfig()
+	url := cfg.BuildDatabaseURL()
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "connect to database: %s", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var accountId uint
+	err = conn.QueryRow(
+		context.Background(),
+		"select id from account where name = $1",
+		username).Scan(&accountId)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "selecting account id by username: %v", err)
+		os.Exit(1)
+	}
+
+	_, err = conn.Exec(context.Background(),
+		"insert into api_key (account_id, zone, api_key, api_secret) values ($1, $2, $3, $4)",
+		accountId, zone, apiKey, apiSecret)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "inserting api key: %v", err)
+		os.Exit(1)
+	}
+}
